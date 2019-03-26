e8e6fe7593bb3bbd0a80d3246426769deaa514bb
Kananek Thongkam <info.dvgamer@gmail.com>
2019-03-26 18:33:38 +0700

    summit connection with local.

--- JDBC-Derby/JDBC-Derby/Program.cs
+++ JDBC-Derby/JDBC-Derby/Program.cs
@@ -1,6 +1,8 @@
 ﻿using java.lang;
 using java.sql;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 
@@ -10,19 +12,64 @@ namespace JDBC_Derby
     {
         static void Main(string[] args)
         {
+            DerbytTest2("C:/apache/derby-db");
+        }
+
+        //static void DerbytTest1(string dbpath)
+        //{
+        //    DerbyNET derbyDB = new DerbyNET(dbpath + ";create=true"); // don't use "\" in directory
+        //    if (!derbyDB.openConnection())
+        //    {
+        //        Console.WriteLine(derbyDB.getLastError());
+        //        Console.ReadKey();
+        //        //Error opening Derby DB
+        //        return;
+        //    }
+        //    DataTable oDT = derbyDB.getRS("SELECT * FROM schemaExample.tblExample") as DataTable;
+        //    if (oDT.Columns[0].ColumnName == "Erro")
+        //    {
+        //        //Read the ** ERRORS ** section below
+        //        Thread.sleep(1000);
+        //        oDT = derbyDB.getRS("SELECT * FROM schemaExample.tblExample") as DataTable;
+        //    }
+        //    else if (oDT.Rows.Count == 1 && oDT.Columns.Count == 1)
+        //    {
+        //        //Some error occured
+        //        Console.WriteLine(derbyDB.getLastError());
+        //        Console.ReadKey();
+        //        return;
+        //    }
+        //    derbyDB.closeConnection();
+        //}
+        static void DerbytTest2(string dbpath)
+        {
+            Console.WriteLine("Set Property: derby.system.home");
+            java.lang.System.setProperty("derby.system.home", dbpath);
+            Console.WriteLine("Register Driver: EmbeddedDriver");
             DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());
-            Connection derbyConn = DriverManager.getConnection(@"jdbc:derby:D:\_SCO\SCO Tools\Razor 64 bit + Key\db-derby-10.10.2.0-bin\derby-db\security;create=true");
-            Statement sta = derbyConn.createStatement();
-            ResultSet res = sta.executeQuery("SELECT * FROM tablename");
-            java.lang.System.@out.println("tablename: ");
+            Console.WriteLine("Connection: jdbc:derby:security");
+            Connection derbyConn = DriverManager.getConnection(@"jdbc:derby:security;create=true");
+
+            Console.WriteLine("Create Statement... (ITEM_SEC)");
+            Statement sta = derbyConn.createStatement(); 
+            ResultSet res = sta.executeQuery("SELECT * from ITEM_SEC");
+            ResultSetMetaData rsmd = res.getMetaData();
+            Console.WriteLine("List Columns: ITEM_SEC");
+            for (var i = 1; i <= rsmd.getColumnCount() - 1; i++)
+            {
+                Console.WriteLine("- Column: {0}", rsmd.getColumnName(i));
+            }
+
+            res = sta.executeQuery("SELECT COUNT(*) nTotal from ITEM_SEC");
             while (res.next())
             {
-
-                java.lang.System.@out.println("  " + res.getString("1stColumn") + ", " + res.getString("2ndColumn"));
+                Console.WriteLine("ITEM_SEC ({0} rows)", res.getString("nTotal"));
             }
-            res.close()
+
+            res.close();
             sta.close();
             derbyConn.close();
+            Console.ReadKey();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 ### I have figured out the issue.
 
-1. two libraries `derbytools.jar` and `derby.jar` have to be compiled via ikvmc to `.dll` and add as reference 
-2. need to add NuGet package `IKVM.OpenJDK.Jdbc` and `IKVM.OpenJDK.Core `
+1. two libraries `derbytools.jar` and `derby.jar` have to be compiled via [ikvmc](https://sourceforge.net/projects/ikvm/files/) to `.dll` and add as reference 
+2. need to add NuGet package `IKVM.OpenJDK.Jdbc`, `IKVM.OpenJDK.Core` and `IKVM.OpenJDK.Management`
 3. current code is `C#`
-
